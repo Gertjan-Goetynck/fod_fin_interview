6d818ae7e82f237579046a93af6d89f0b446e244
Gertjan Goetynck <Gertjan@goetynck.com>
2020-10-29 02:04:20 +0100

    Changed form focus colors

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -3,20 +3,28 @@
     <h1 style="color: white">Contact me</h1>
     <v-card
       ><form
-        name="contact"
+        name="contactForm"
         method="post"
         netlify
         data-netlify-honeypot="bot-field"
       >
-        <input type="hidden" name="form-name" value="contact" />
+        <input type="hidden" name="form-name" value="contactForm" />
 
         <v-container>
           <v-row>
             <v-col cols="12" md="6">
-              <v-text-field label="First name" name="firstName"></v-text-field>
+              <v-text-field
+                color="#00A2A2"
+                label="First name"
+                name="firstName"
+              ></v-text-field>
             </v-col>
             <v-col cols="12" md="6">
-              <v-text-field label="Last name" name="lastName"></v-text-field>
+              <v-text-field
+                color="#00A2A2"
+                label="Last name"
+                name="lastName"
+              ></v-text-field>
             </v-col>
           </v-row>
           <v-row>
@@ -25,17 +33,26 @@
                 label="E-mail"
                 type="email"
                 name="email"
+                color="#00A2A2"
               ></v-text-field>
             </v-col>
           </v-row>
           <v-row>
             <v-col cols="12" md="6">
-              <v-text-field label="Subject" name="subject"></v-text-field>
+              <v-text-field
+                color="#00A2A2"
+                label="Subject"
+                name="subject"
+              ></v-text-field>
             </v-col>
           </v-row>
           <v-row>
             <v-col cols="12" md="6">
-              <v-textarea label="Message" name="message"></v-textarea>
+              <v-textarea
+                color="#00A2A2"
+                label="Message"
+                name="message"
+              ></v-textarea>
             </v-col>
           </v-row>
           <v-row>
