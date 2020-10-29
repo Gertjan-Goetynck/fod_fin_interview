3dad66513f9f6979fde12d5f972849965e84f989
Gertjan Goetynck <Gertjan@goetynck.com>
2020-10-29 01:21:29 +0100

    Added dummy form for netlify

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -2,7 +2,7 @@
   <div>
     <h1 style="color: white">Contact me</h1>
     <v-card
-      ><v-form
+      ><form
         name="contact"
         method="post"
         data-netlify="true"
@@ -13,25 +13,29 @@
         <v-container>
           <v-row>
             <v-col cols="12" md="6">
-              <v-text-field label="First name"></v-text-field>
+              <v-text-field label="First name" name="firstName"></v-text-field>
             </v-col>
             <v-col cols="12" md="6">
-              <v-text-field label="Last name"></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="12" md="6">
-              <v-text-field label="E-mail" type="email"></v-text-field>
+              <v-text-field label="Last name" name="lastName"></v-text-field>
             </v-col>
           </v-row>
           <v-row>
             <v-col cols="12" md="6">
-              <v-text-field label="Subject"></v-text-field>
+              <v-text-field
+                label="E-mail"
+                type="email"
+                name="email"
+              ></v-text-field>
             </v-col>
           </v-row>
           <v-row>
             <v-col cols="12" md="6">
-              <v-textarea label="Message"></v-textarea>
+              <v-text-field label="Subject" name="subject"></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12" md="6">
+              <v-textarea label="Message" name="message"></v-textarea>
             </v-col>
           </v-row>
           <v-row>
@@ -42,7 +46,7 @@
             >
           </v-row>
         </v-container>
-      </v-form></v-card
+      </form></v-card
     >
   </div>
 </template>
